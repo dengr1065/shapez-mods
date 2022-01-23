320d680d4e3b6dc3cfa25c5935f6b5a3b1db14c4
Даниїл Григор'єв <dengr1065@gmail.com>
2022-01-23 07:47:16 +0200

    Cache color-coded building sprites, game update

--- src/colorcoded/system_patches.js
+++ src/colorcoded/system_patches.js
@@ -1,20 +1,67 @@
 // Patches to StaticMapEntity system to apply filters
 
 import { globalConfig } from "core/config";
+import {
+    AtlasSprite,
+    ORIGINAL_SPRITE_SCALE,
+    SpriteAtlasLink
+} from "core/sprites";
+
+/**
+ * @param {AtlasSprite} source
+ * @param {string} filter
+ */
+function createSprite(source, filter) {
+    const sourceLink = source.linksByResolution[ORIGINAL_SPRITE_SCALE];
+    const { w, h } = sourceLink;
+
+    const sprite = new AtlasSprite(source.spriteName);
+
+    const canvas = document.createElement("canvas");
+    canvas.width = w;
+    canvas.height = h;
+
+    const ctx = canvas.getContext("2d");
+    ctx.filter = filter;
+    source.draw(ctx, 0, 0, w, h);
+
+    const link = new SpriteAtlasLink({
+        w,
+        h,
+        atlas: canvas,
+        packOffsetX: 0,
+        packOffsetY: 0,
+        packedX: 0,
+        packedY: 0,
+        packedW: w,
+        packedH: h
+    });
+
+    for (const resolution of ["0.25", "0.5", "0.75"]) {
+        sprite.linksByResolution[resolution] = link;
+    }
+
+    return sprite;
+}
 
 function drawSpriteWithFilter(entity, parameters, sprite) {
     const { StaticMapEntity, ColorCoded } = entity.components;
-    const filter = ColorCoded.hasColorFilter();
 
-    if (filter) {
-        parameters.context.filter = ColorCoded.getColorFilter();
+    if (ColorCoded.hasColorFilter()) {
+        if (!sprite.colorCoded) {
+            sprite.colorCoded = [];
+        }
+
+        const filter = ColorCoded.getColorFilter();
+        if (!sprite.colorCoded[filter]) {
+            // create a sprite with applied filter and cache it
+            sprite.colorCoded[filter] = createSprite(sprite, filter);
+        }
+
+        sprite = sprite.colorCoded[filter];
     }
 
     StaticMapEntity.drawSpriteOnBoundsClipped(parameters, sprite, 2);
-
-    if (filter) {
-        parameters.context.filter = "none";
-    }
 }
 
 export function drawChunk(_, [parameters, chunk]) {
