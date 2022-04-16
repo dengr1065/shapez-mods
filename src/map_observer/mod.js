25c1b7e31b8f228ff8c5dab1e392aca0d4355800
Даниїл Григор'єв <dengr1065@gmail.com>
2022-04-17 02:56:03 +0300

    Map Observer: Grid theming, 0.2.0

--- src/map_observer/mod.js
+++ src/map_observer/mod.js
@@ -1,42 +1,91 @@
 import { globalConfig } from "core/config";
+import { THEMES } from "game/theme";
 import { Mod } from "mods/mod";
 import { StorageImplElectron } from "platform/electron/storage";
 import info from "./mod.json";
+import { MapObserverSettingsState } from "./settings";
+import settingsCSS from "./settings.less";
 
 const defaultSettings = {
-    _comment:
-        "Value below will be applied to the game. (vanilla = 0.9, default = 0.6)",
-    minZoom: 0.6
+    minZoom: 0.6,
+    customizeGrid: false,
+    gridBackground: "#1a2b19",
+    gridForeground: "#243d23"
 };
+
+const vanillaThemeMapColors = {};
 
 class MapObserver extends Mod {
     init() {
+        this.modInterface.registerCss(settingsCSS);
+
         this.storage = new StorageImplElectron(this.app);
-        this.prepareSettings().then(() => this.setMinZoom());
+        this.settingsFile = this.metadata.id + "_settings.json";
+        this.saveSettings = () => this.saveCustomSettings();
+        this.prepareSettings();
+
+        this.signals.appBooted.add(() => {
+            // Store vanilla theme colors so we can disable the customization
+            for (const theme in THEMES) {
+                vanillaThemeMapColors[theme] = {
+                    background: THEMES[theme].map.background,
+                    grid: THEMES[theme].map.grid
+                };
+            }
+
+            this.modInterface.registerGameState(MapObserverSettingsState);
+            this.setConfig();
+        });
     }
 
     async prepareSettings() {
-        const file = this.metadata.id + "_settings.json";
         this.settings = defaultSettings;
 
         try {
             // Manually read settings from the file
-            const stored = await this.storage.readFileAsync(file);
+            const stored = await this.storage.readFileAsync(this.settingsFile);
             this.settings = JSON.parse(stored);
+
+            // Set missing settings keys
+            for (const key in defaultSettings) {
+                if (!(key in this.settings)) {
+                    this.settings[key] = defaultSettings[key];
+                }
+            }
         } catch (err) {
             // Settings file doesn't exist yet (or is corrupt)
             const data = this.serializeSettings(this.settings);
-            await this.storage.writeFileAsync(file, data);
+            await this.storage.writeFileAsync(this.settingsFile, data);
         }
+    }
+
+    async saveCustomSettings() {
+        const data = this.serializeSettings(this.settings);
+        await this.storage.writeFileAsync(this.settingsFile, data);
     }
 
     serializeSettings(data) {
         return JSON.stringify(data, undefined, 2);
     }
 
-    setMinZoom() {
+    setConfig() {
         // This is a "single line" mod, but it's useful for some people.
         globalConfig.mapChunkOverviewMinZoom = this.settings.minZoom ?? 0.6;
+
+        if (this.settings.customizeGrid) {
+            // Set our customized colors
+            for (const theme in THEMES) {
+                THEMES[theme].map.background = this.settings.gridBackground;
+                THEMES[theme].map.grid = this.settings.gridForeground;
+            }
+        } else {
+            // Restore vanilla colors
+            for (const theme in THEMES) {
+                const vanilla = vanillaThemeMapColors[theme];
+                THEMES[theme].map.background = vanilla.background;
+                THEMES[theme].map.grid = vanilla.grid;
+            }
+        }
     }
 }
 
---
+++ src/map_observer/settings.js
@@ -0,0 +1,88 @@
+import { TextualGameState } from "core/textual_game_state";
+import { makeDiv } from "core/utils";
+import { MODS } from "mods/modloader";
+
+function labelWrap(text, element) {
+    const label = document.createElement("label");
+    label.innerText = text;
+    label.appendChild(element);
+
+    return label;
+}
+
+function makeCheckboxInput(checked) {
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.checked = checked;
+
+    return input;
+}
+
+function makeColorInput(color) {
+    const input = document.createElement("input");
+    input.type = "color";
+    input.value = color;
+
+    return input;
+}
+
+export class MapObserverSettingsState extends TextualGameState {
+    constructor() {
+        super("MapObserverSettingsState");
+
+        /** @type {import("mods/mod").Mod} */
+        this.mod = MODS.mods.find(
+            (mod) => mod.metadata.id == "dengr1065:map_observer"
+        );
+    }
+
+    onEnter() {
+        const settings = this.mod.settings;
+
+        const customizeGrid = makeCheckboxInput(settings.customizeGrid);
+        const backgroundInput = makeColorInput(settings.gridBackground);
+        const foregroundInput = makeColorInput(settings.gridForeground);
+
+        customizeGrid.addEventListener("change", () => {
+            console.log("Change fires");
+            settings.customizeGrid = customizeGrid.checked;
+        });
+
+        backgroundInput.addEventListener("change", () => {
+            settings.gridBackground = backgroundInput.value;
+        });
+
+        foregroundInput.addEventListener("change", () => {
+            settings.gridForeground = foregroundInput.value;
+        });
+
+        const content = document.querySelector(".mainContent");
+        makeDiv(content, undefined, [], "Settings");
+
+        const zoomOutLimitHint = document.createElement("p");
+        zoomOutLimitHint.innerHTML = [
+            "Modifying level of zooming out for Map View to turn on is",
+            "currently impossible in the settings. Please manually edit",
+            "the configuration file. Sorry for inconvenience!"
+        ].join("<br>");
+        content.appendChild(zoomOutLimitHint);
+
+        content.appendChild(labelWrap("Customize Grid Colors", customizeGrid));
+        content.appendChild(labelWrap("Background Color", backgroundInput));
+        content.appendChild(labelWrap("Grid Lines", foregroundInput));
+    }
+
+    getStateHeaderTitle() {
+        return "Map Observer";
+    }
+
+    onLeave() {
+        this.mod.saveSettings();
+        this.mod.setConfig();
+    }
+
+    onBeforeExit() {
+        // lies!!!
+        this.onLeave();
+    }
+}
