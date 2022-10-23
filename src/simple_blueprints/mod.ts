a2ac232d11ca2ec66ebf63e2376e4f877a6350ec
Даниїл Григор'єв <dengr1065@gmail.com>
2022-10-23 16:28:50 +0300

    Simple Blueprints: Release

---
+++ src/simple_blueprints/mod.ts
@@ -0,0 +1,51 @@
+import { DrawParameters } from "core/draw_utils";
+import { HUDBlueprintPlacer } from "game/hud/parts/blueprint_placer";
+import { THEMES } from "game/theme";
+import { Mod } from "mods/mod";
+import _metadata from "./mod.json";
+import { drawBlueprint } from "./renderer";
+import icon from "./icon.webp";
+
+export function drawPatch(
+    this: HUDBlueprintPlacer,
+    srcMethod: (parameters: DrawParameters) => void,
+    [parameters]: [DrawParameters]
+) {
+    if (!this.root.camera.getIsMapOverlayActive()) {
+        return srcMethod(parameters);
+    }
+
+    const blueprint = this.currentBlueprint.get();
+    if (!blueprint) {
+        return;
+    }
+
+    const mousePosition = this.root.app.mousePosition;
+    if (!mousePosition) {
+        // Not on screen
+        return;
+    }
+
+    const worldPos = this.root.camera.screenToWorld(mousePosition);
+    const tile = worldPos.toTileSpace();
+    drawBlueprint(parameters, blueprint, tile);
+}
+
+class SimpleBlueprints extends Mod {
+    init() {
+        this.modInterface.replaceMethod(HUDBlueprintPlacer, "draw", drawPatch);
+        this.signals.appBooted.add(this.modifyThemes, this);
+    }
+
+    modifyThemes() {
+        for (const themeId in THEMES) {
+            THEMES[themeId][this.metadata.id] = {
+                blueprintColor: this.settings.blueprintColor
+            };
+        }
+    }
+}
+
+const metadata = _metadata as unknown as ModExtrasMetadata;
+metadata.extra.icon = icon;
+registerMod(SimpleBlueprints, metadata);
---
+++ src/simple_blueprints/renderer.ts
@@ -0,0 +1,81 @@
+import { globalConfig } from "core/config";
+import { DrawParameters } from "core/draw_utils";
+import { Vector } from "core/vector";
+import { Blueprint } from "game/blueprint";
+import { MetaBuilding } from "game/meta_building";
+import metadata from "./mod.json";
+
+const PIXEL_SIZE = globalConfig["tileSize"] / 3;
+
+function drawMatrix(
+    { context }: DrawParameters,
+    matrix: number[] | null,
+    tile: Vector
+) {
+    const worldTile = tile.toWorldSpace();
+    for (let dx = 0; dx < 3; ++dx) {
+        for (let dy = 0; dy < 3; ++dy) {
+            const isFilled = matrix[dx + dy * 3];
+            if (isFilled) {
+                context.fillRect(
+                    worldTile.x + dx * PIXEL_SIZE,
+                    worldTile.y + dy * PIXEL_SIZE,
+                    PIXEL_SIZE,
+                    PIXEL_SIZE
+                );
+            }
+        }
+    }
+}
+
+export function drawBlueprint(
+    parameters: DrawParameters,
+    blueprint: Blueprint,
+    tile: Vector
+) {
+    // FIXME: refactor to use getMod
+    parameters.context.fillStyle =
+        window["shapez"]["THEME"][metadata.id]["blueprintColor"];
+
+    for (const entity of blueprint.entities) {
+        const staticComp = entity.components.StaticMapEntity;
+        const newPos = staticComp.origin.add(tile);
+
+        const rect = staticComp.getTileSpaceBounds();
+        rect.moveBy(tile.x, tile.y);
+
+        const metaBuilding: MetaBuilding = staticComp.getMetaBuilding();
+        const variant = staticComp.getVariant();
+        const rotationVariant = staticComp.getRotationVariant();
+
+        const canPlace = parameters.root.logic.checkCanPlaceEntity(entity, {
+            offset: tile
+        });
+        parameters.context.globalAlpha = canPlace ? 1 : 0.3;
+
+        const matrix = metaBuilding.getSpecialOverlayRenderMatrix(
+            staticComp.rotation,
+            rotationVariant,
+            variant,
+            entity
+        );
+
+        if (matrix) {
+            drawMatrix(parameters, matrix, newPos);
+        } else {
+            const worldTile = newPos.toWorldSpace();
+            const size = staticComp
+                .getTileSize()
+                .rotateFastMultipleOf90(staticComp.rotation);
+
+            parameters.context.fillRect(
+                worldTile.x,
+                worldTile.y,
+                globalConfig["tileSize"] * size.x,
+                globalConfig["tileSize"] * size.y
+            );
+        }
+    }
+
+    parameters.context.globalAlpha = 1;
+}
